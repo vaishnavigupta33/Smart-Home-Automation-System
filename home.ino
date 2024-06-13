2d46165dc7c863d45c9dfe440fc5b178cdab39e7
Vaishnavi Gupta <108528485+vaishnavigupta33@users.noreply.github.com>
2024-06-13 22:44:19 +0530

    Add files via upload

---
+++ home.ino
@@ -0,0 +1,25 @@
+void setup() {
+  pinMode(13,OUTPUT);
+  pinMode(12,OUTPUT);
+  pinMode(11,OUTPUT);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(13,HIGH);
+  delay(4000);
+  digitalWrite(13,LOW);
+  delay(4000);
+  digitalWrite(12,HIGH);
+  delay(4000);
+  digitalWrite(12,LOW);
+  delay(4000);
+  digitalWrite(11,HIGH);
+  delay(4000);
+  digitalWrite(11,LOW);
+  delay(4000);
+  
+  // put your main code here, to run repeatedly:
+
+}
